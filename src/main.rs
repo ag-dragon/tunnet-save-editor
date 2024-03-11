8cb72889f1359b45f966323be97112f9e96ddcd9
ag-dragon <h28941+github@gmail.com>
2024-03-11 18:33:51 -0400

    Move journal struct/enum/ui to journal.rs

--- src/main.rs
+++ src/main.rs
@@ -93,106 +93,6 @@ enum Annotation {
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-struct Knowledge {
-    unread: bool,
-    journal: Vec<JournalEntry>,
-}
-
-#[derive(EnumIter, Serialize, Deserialize, PartialEq, Clone, Copy, Debug)]
-enum JournalEntry {
-    Journal,
-    LongHibernation,
-    JobDescription,
-    RelayAvailable,
-    HubAvailable,
-    Filter,
-    FilterAvailable,
-    Antivirus,
-    AntivirusAvailable,
-    Combo,
-    ConnectionStatus,
-    CannotReachSurface,
-    RobotsHaveAFearModule,
-    RobotsCannotSurviveAtTheSurface,
-    OtherMainframesArePresent,
-    Antenna,
-    LongRangeScanner,
-    Tester,
-    OwnCabin,
-    SupervisorWokeTechnicianUp,
-    SupervisorWasHiredByTheArchitects,
-    SupervisorName,
-    SupervisorDoesNotRespond,
-    SupervisorNeedToFind,
-    SupervisorNeedsHelp,
-    SupervisorRadio,
-    SupervisorExploresDNASequence,
-    ArchitectsExist,
-    ArchitectsCreatedTheBunkers,
-    ArchitectsCreatedTheRobots,
-    ArchitectsDisappeared,
-    ArchitectsDidNotFinish,
-    ArchitectsFoundLifeForm,
-    MadelineTrappedTheArchitects,
-    ArchitectsDiedInHQ,
-    Apocalypse,
-    ApocalypseDidNotHappen,
-    Infection,
-    InfectionEffects,
-    Stalker,
-    Drone,
-    DroneShowsDirection,
-    DroneNeedsHelp,
-    CookSellsBatteries,
-    FishermanSellsScrap,
-    FishermanHasAMagnet,
-    FishermanLentMeHisMagnet,
-    FarmerTradesVegetablesForSeeds,
-    HermitLooksForADisk,
-    HermitTradesOilForVegetables,
-    GrandmaTradesToysForOil,
-    GrandmaNetworkUsage,
-    GrandpaNeedsABattery,
-    GrandpaHasANewBattery,
-    GrandpaWantsToHelpGrandson,
-    KidWantsAToy,
-    SoldiersGotTheMagnets,
-    SoldiersNeedOil,
-    OperatorNeedsAScrew,
-    OperatorTradesSpeakersForMagnets,
-    OperatorCanWork,
-    GrandsonJoinedTheScout,
-    ScoutTradesSeedsForScraps,
-    DJTradesDisksForSpeakers,
-    CashierBuysAnything,
-    SpidersDoNotLikeLight,
-    MonksShouldNotBeLookedAt,
-    SirensShouldBeLookedAt,
-    AbandonedLabQuiet,
-    UndergroundComplex,
-    ScoutsAreHiring,
-    GrandsonAgreedToJoin,
-    GrandsonExists,
-    VIPCodePhrase,
-    VIPCode,
-    NetworkUsageScientist,
-    MadelineIsMad,
-    MadelineHasAnIdea,
-    MadelineTookBattery,
-    MadelineNeedsBattery,
-    TechnicanNameDefault,
-    TechnicianNameHarry,
-    TechnicianNameRoger,
-    TechnicianNameTherese,
-    TechnicianNameChantal,
-    DJNetworkUsage,
-    DJHasASpeaker,
-    ShelterNetworkUsage,
-    BossCanDisinfect,
-    Retire,
-}
-
-#[derive(Serialize, Deserialize, Default, Debug)]
 struct Home {
     items: Vec<Furniture>,
     ads: Vec<Ad>,
@@ -322,7 +222,7 @@ struct Story {
     pages: i32,
 
     inventory: player::inventory::Inventory,
-    knowledge: Knowledge,
+    knowledge: player::journal::Knowledge,
     home: Home,
     visited_chunks: Vec<ChunkCoords>,
     map_annotations: MapAnnotations,
@@ -658,27 +558,7 @@ fn editor_ui(mut contexts: EguiContexts, mut editor_tab: ResMut<EditorTab>, mut 
                             });
 
                             player::inventory::inventory_editor(ui, &mut save_file);
-
-                            ui.collapsing("Journal", |ui| {
-                                ui.horizontal(|ui| {
-                                    let mut unread = save_file.story.knowledge.unread;
-                                    ui.checkbox(&mut unread, "Unread Symbol");
-                                    save_file.story.knowledge.unread = unread;
-                                });
-                                ui.collapsing("Entries", |ui| {
-                                    for entry in JournalEntry::iter() {
-                                        ui.horizontal(|ui| {
-                                            let mut note = save_file.story.knowledge.journal.contains(&entry);
-                                            ui.checkbox(&mut note, format!("{:?}", entry));
-                                            if !note && save_file.story.knowledge.journal.contains(&entry) {
-                                                save_file.story.knowledge.journal.retain(|&x| x != entry);
-                                            } else if note && !save_file.story.knowledge.journal.contains(&entry) {
-                                                save_file.story.knowledge.journal.push(entry);
-                                            }
-                                        });
-                                    }
-                                });
-                            });
+                            player::journal::journal_editor(ui, &mut save_file);
 
                             ui.collapsing("Guidebook", |ui| {
                                 // page_no
