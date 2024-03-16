fd3cfe2e519b972d772c0c668f9b1cd5b5e967dc
ag-dragon <h28941+github@gmail.com>
2024-03-16 13:30:10 -0400

    Make ChunkTypes support rooms with addresses

--- src/chunks/rooms.rs
+++ src/chunks/rooms.rs
@@ -1,11 +1,42 @@
 use serde::{Serialize, Deserialize};
 
 use crate::chunks::ChunkCoords;
+use crate::network::Address;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum ChunkRoom {
     CloisterCeiling,
     SwimmingPool,
+    MainframeRoom(Address),
+    CommandRoom(Address),
+    Church(Address),
+    MainframeRoomLuxury(Address),
+    FilterLab(Address),
+    HubLab(Address),
+    AntennaLab(Address),
+    Hermit(Address),
+    School(Address),
+    PartyShelter(Address),
+    TesterLab(Address),
+    Farm(Address),
+    Elderlies(Address),
+    PoorShelter(Address),
+    ServerRoom(Address),
+    Workshop(Address),
+    MilitaryOutpost(Address),
+    Beach(Address),
+    Cave(Address),
+    Supermarket(Address),
+    Shelter(Address),
+    Camp(Address),
+    MainframeRoomAbandonedLab(Address),
+    AbandonedLabNorthWest(Address),
+    AbandonedLabNorthEast(Address),
+    AbandonedLabSouthWest(Address),
+    AbandonedLabSouthEast(Address),
+
+    #[serde(other)]
+    Unknown,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
